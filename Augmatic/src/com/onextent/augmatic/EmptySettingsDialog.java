5f848d84bc779bba798d326bf4be7177d78bf66e
Ed Sweeney <ed@onextent.com>
2013-01-27 15:18:52 -0800

    refactor camera list for layout usage

---
+++ Augmatic/src/com/onextent/augmatic/EmptySettingsDialog.java
@@ -0,0 +1,21 @@
+package com.onextent.augmatic;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.actionbarsherlock.app.SherlockDialogFragment;
+import com.onextent.augmatic.R;
+
+public class EmptySettingsDialog extends SherlockDialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        Dialog d = getDialog();
+        //if (d != null) d.setTitle("No Settings Available");
+        View v = inflater.inflate(R.layout.empty_settings, container, false);
+        return v;
+    }
+}
